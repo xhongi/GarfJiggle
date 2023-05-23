2c524bc6e147fbb66b2084d46e90cd2239991e5b
xhongi <xhongi.poe@gmail.com>
2023-05-23 23:13:47 +0200

    basic chatbot

---
+++ chatbot/bot.py
@@ -0,0 +1,71 @@
+from twitchAPI import Twitch
+from twitchAPI.oauth import UserAuthenticator
+from twitchAPI.types import AuthScope, ChatEvent
+from twitchAPI.chat import Chat, EventData, ChatMessage, ChatSub, ChatCommand
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+import asyncio
+
+dotenv_path = Path('.env')
+load_dotenv(dotenv_path=dotenv_path)
+
+APP_ID = os.getenv('TWITCH_CLIENT_ID')
+APP_SECRET = os.getenv('TWITCH_SECRET')
+USER_SCOPE = [AuthScope.CHAT_READ]
+TARGET_CHANNEL = 'ruetoo'
+
+garfers = ['garf']
+jigglers = ['WidestJigglin']
+
+
+# this will be called when the event READY is triggered, which will be on bot start
+async def on_ready(ready_event: EventData):
+    print('Bot is ready for work, joining channels')
+    # join our target channel, if you want to join multiple, either call join for each individually
+    # or even better pass a list of channels as the argument
+    await ready_event.chat.join_room(TARGET_CHANNEL)
+    # you can do other bot initialization things in here
+
+
+# this will be called whenever a message in a channel was send by either the bot OR another user
+async def on_message(msg: ChatMessage):
+    if msg.text in jigglers:
+        print(f'Jiggler detected: {msg.user.name}')
+    if msg.text in garfers:
+        print(f'Garfer detected: {msg.user.name}')
+
+# this is where we set up the bot
+async def run():
+    # set up twitch api instance and add user authentication with some scopes
+    twitch = await Twitch(APP_ID, APP_SECRET)
+    auth = UserAuthenticator(twitch, USER_SCOPE)
+    token, refresh_token = await auth.authenticate()
+    await twitch.set_user_authentication(token, USER_SCOPE, refresh_token)
+
+    # create chat instance
+    chat = await Chat(twitch)
+
+    # register the handlers for the events you want
+
+    # listen to when the bot is done starting up and ready to join channels
+    chat.register_event(ChatEvent.READY, on_ready)
+    # listen to chat messages
+    chat.register_event(ChatEvent.MESSAGE, on_message)
+    # listen to channel subscriptions
+
+
+    # we are done with our setup, lets start this bot up!
+    chat.start()
+
+    # lets run till we press enter in the console
+    try:
+        input('press ENTER to stop\\n')
+    finally:
+        # now we can close the chat bot and the twitch api client
+        chat.stop()
+        await twitch.close()
+
+
+# lets run our setup
+asyncio.run(run())
